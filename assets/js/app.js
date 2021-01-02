43516a1bf4c826b632095b93bac5389bcd6909d3
Andy Williams <andy@andywilliams.me>
2021-01-02 12:22:05 +0000

    Mark incompatible apps as incompatible, based on current system

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+function filter(os) {
+    $(".app").each(function(index) {
+        if (!$(this).hasClass("requires")) {
+            return;
+        }
+        if ($(this).hasClass(os)) {
+            return;
+        }
+        
+        $(this).addClass(" incompatible");
+    });
+}
+
+$(document).ready(function(){
+    var ua = navigator.userAgent || navigator.vendor || window.opera;
+    if (/android/i.test(ua)) {
+        filter("android");
+        return;
+    } else if (/iPad|iPhone|iPod/.test(ua) && !window.MSStream) {
+        filter("ios");
+        return;
+    }
+
+    var os = window.navigator.platform;
+    if (os == "win32") {
+        filter("windows");
+    } else if (os == "MacIntel") {
+        filter("macos");
+    } else if (os == "Linux i686" || os == "Linux x86_64" ||
+               os == "FreeBSD i386" || os == "FreeBSD amd64") {
+        filter("linux");
+    }
+});
